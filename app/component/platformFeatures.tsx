6d727093a08fa7a04aa3e1eeba587aee462acdcd
Iroegbu David <iroegbu.dgit@gmail.com>
2024-06-07 15:28:55 +0100

    updated landing page

--- app/component/platformFeatures.tsx
+++ app/component/platformFeatures.tsx
@@ -5,44 +5,44 @@ const PlatformFeatures = () => {
         <div id="platformfeatures" className="w-full min-h-[100vh] flex flex-col items-center justif-start gap-10 pt-[40px] pb-[20px] pr-[70px] pl-[70px] ">
             <h3 className="font-bold text-3xl text-black">Platform Features</h3>
             <div className="w-full flex flex-col justify-start item-center gap-10 h-auto ">
-                <div className="w-full flex flex-row items-start justify-between gap-3 h-auto ">
+                <div className="w-full flex flex-row items-start justify-between gap-3 h-auto">
                     {/* left side */}
                     <div className="w-[65%] flex flex-col items-start justify-start gap-3 ">
                         {/* top */}
                         <div className="w-full flex flex-row items-start justify-between gap-3 bg-">
                             {/* left side */}
-                            <span className=" w-[45%] min-h-[100px] p-[10px] pr-[15px] pl-[15px] flex flex-col justify-start items-between gap-3 bg-gray-100 rounded-[5px] ">
+                            <span className=" w-[45%] min-h-[120px] p-[10px] pr-[15px] pl-[15px] flex flex-col justify-start items-between gap-3 bg-gray-100 rounded-[5px] ">
                                 <h4 className="font-bold text-lg">Projects</h4>
                                 <p className="text-sm font-light">Keep track of the number of new projects</p>
-                                <span className="w-full h-[100px] flex flex-row items-center justify-between bg-red-100 "></span>
+                                <span className="w-full h-[100px] flex flex-row items-center justify-between  "></span>
                             </span>
                             {/* right side */}
-                            <span className=" w-[55%] min-h-[100px] p-[10px] pr-[15px] pl-[15px] flex flex-col justify-start items-between gap-3 bg-gray-100 rounded-[5px] ">
+                            <span className=" w-[55%] min-h-[120px] p-[10px] pr-[15px] pl-[15px] flex flex-col justify-start items-between gap-3 bg-gray-100 rounded-[5px] ">
                                 <h4 className="font-bold text-lg">Projects</h4>
                                 <p className="text-sm font-light">Keep track of the number of new projects</p>
-                                <span className="w-full h-[100px] flex flex-row items-center justify-between bg-red-100 "></span>
+                                <span className="w-full h-[100px] flex flex-row items-center justify-between  "></span>
                             </span>
                         </div>
                         {/* bottom */}
                         <div className="w-full flex flex-row items-start justify-between gap-3">
-                            <span className=" w-[55%] min-h-[100px] p-[10px] pr-[15px] pl-[15px] flex flex-col justify-start items-between gap-3 bg-gray-100 rounded-[5px] ">
+                            <span className=" w-[55%] min-h-[120px] p-[10px] pr-[15px] pl-[15px] flex flex-col justify-start items-between gap-3 bg-gray-100 rounded-[5px] ">
                                 <h4 className="font-bold text-lg">Projects</h4>
                                 <p className="text-sm font-light">Keep track of the number of new projects</p>
-                                <span className="w-full h-[100px] flex flex-row items-center justify-between bg-red-100 "></span>
+                                <span className="w-full h-[100px] flex flex-row items-center justify-between  "></span>
                             </span>
-                            <span className=" w-[45%] min-h-[100px] p-[10px] pr-[15px] pl-[15px] flex flex-col justify-start items-between gap-3 bg-gray-100 rounded-[5px] ">
+                            <span className=" w-[45%] min-h-[120px] p-[10px] pr-[15px] pl-[15px] flex flex-col justify-start items-between gap-3 bg-gray-100 rounded-[5px] ">
                                 <h4 className="font-bold text-lg">Projects</h4>
                                 <p className="text-sm font-light">Keep track of the number of new projects</p>
-                                <span className="w-full h-[100px] flex flex-row items-center justify-between bg-red-100 "></span>
+                                <span className="w-full h-[100px] flex flex-row items-center justify-between  "></span>
                             </span>
                         </div>
                     </div>
                     {/* right side */}
-                    <div className="w-[35%] flex flex-col items-start justify-start gap-3">
-                        <span className=" w-[100%] min-h-[100px] p-[10px] pr-[15px] pl-[15px] flex flex-col justify-start items-between gap-3 bg-gray-100 rounded-[5px] ">
+                    <div className="w-[35%] h-[395px] flex flex-col items-start justify-start gap-3">
+                        <span className=" w-[100%] h-full p-[10px] pr-[15px] pl-[15px] flex flex-col justify-start items-between gap-3 bg-gray-100 rounded-[5px] ">
                             <h4 className="font-bold text-lg">Projects</h4>
                             <p className="text-sm font-light">Keep track of the number of new projects</p>
-                            <span className="w-full h-[100px] flex flex-row items-center justify-between bg-blue-100 "></span>
+                            <span className="w-full h-[100px] flex flex-row items-center justify-between  "></span>
                         </span>
                     </div>
                 </div>
@@ -50,9 +50,9 @@ const PlatformFeatures = () => {
                     <h3 className="font-semibold text-2xl text-black">Upgrade plan for additional features</h3>
                     <div className="w-full flex flex-row items-start justify-between gap-3">
                         {/* left side to be an image of a hand pressing a laptop */}
-                        <span className="w-[40%] h-[400px] rounded-[5px] bg-blue-100 "></span>
+                        <span className="w-[40%] h-[400px] rounded-[5px] bg-gray-100 "></span>
                         {/* right side listing the additional features */}
-                        <div className="w-[60%] h-[400px] bg-blue-500 rounded-[5px] "></div>
+                        <div className="w-[60%] h-[400px] bg-gray-100 rounded-[5px] "></div>
                     </div>
                 </div>
 
--- app/component/reviews.tsx
+++ app/component/reviews.tsx
@@ -6,13 +6,13 @@ const Reviews = () => {
             <h3 className="font-bold text-3xl text-black">Reviews</h3>
             <div className="w-full flex flex-row items-start justify-between gap-3 h-auto">
                 {/* left side */}
-                <div className="w-[30%] flex flex-col items-start justify-start gap-3 h-[500px] bg-blue-200 "></div>
+                <div className="w-[30%] flex flex-col items-start justify-start gap-3 h-[500px] bg-gray-100 "></div>
                 <div className="w-[40%] flex flex-col items-start justify-start gap-3 h-[500px] ">
-                    <div className="w-full h-1/2 bg-amber-200 "></div>
-                    <div className="w-full h-1/2 bg-amber-200 "></div>
+                    <div className="w-full h-1/2 bg-gray-100 "></div>
+                    <div className="w-full h-1/2 bg-gray-100 "></div>
                 </div>
                 {/* right side */}
-                <div className="w-[30%] flex flex-col items-start justify-start gap-3 h-[500px] bg-red-200 "></div>
+                <div className="w-[30%] flex flex-col items-start justify-start gap-3 h-[500px] bg-gray-100 "></div>
             </div>
 
         </div>
