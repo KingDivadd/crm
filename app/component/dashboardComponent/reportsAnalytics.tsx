6d21448c2277ce6b28609df329d8e8ec1447eb5a
Iroegbu David <iroegbu.dgit@gmail.com>
2024-06-10 10:29:52 +0100

    updated reports and analytics

--- app/component/dashboardComponent/reportsAnalytics.tsx
+++ app/component/dashboardComponent/reportsAnalytics.tsx
@@ -341,6 +341,146 @@ const ReportsAndAnalytics = () => {
                     </div>
                     {/* sales analytics */}
                 </div>
+
+                {/* Employee Attendance */}
+                <div className="w-full h-auto flex flex-col items-start justify-start gap-[10px]">
+                    <p className="text-xl font-semibold">Employee Attandance </p>
+                    <div className="w-full min-h-[150px] flex flex-col bg-white rounded-[5px] border border-blue-500 ">
+                            <span className="w-full h-[40px] flex flex-col items-start justify-start bg-white rounded-t-[5px] border-b-2 border-gray-200 ">
+                                <span className="w-full h-[40px] flex flex-row items-center justify-start bg-white rounded-t-[5px]">
+                                    <p className="text-sm font-semibold w-[20%] pr-2 pl-2 ">Employee Id</p>
+                                    <p className="text-sm font-semibold w-[20%] pr-2 pl-2 ">Employee Name</p>
+                                    <p className="text-sm font-semibold w-[20%] pr-2 pl-2 ">Date</p>
+                                    <p className="text-sm font-semibold w-[20%] pr-2 pl-2 ">Status</p>
+                                    <p className="text-sm font-semibold w-[20%] pr-2 pl-2 ">Hours Worked</p>
+                                </span>
+                            </span>
+                            <div className="w-full h-[200px] flex flex-col justify-start items-start">
+                                {[1,2,3,4,5].map((data, ind)=>{
+                                    return (
+                                        <span key={ind} className="recent-activity-table-list">
+                                            <p className="text-sm w-[20%] pr-2 pl-2 ">EMP001{ind}</p>
+                                            <p className="text-sm w-[20%] pr-2 pl-2 ">Iroegbu David</p>
+                                            <p className="text-sm w-[20%] pr-2 pl-2 ">09-06-2024</p> 
+                                            <p className="text-sm w-[20%] pr-2 pl-2 text-red-600 ">Absent</p> 
+                                            <p className="text-sm w-[20%] pr-2 pl-2 text-lime-500 ">0</p> 
+                                        </span>
+                                    )
+                                })}
+                            </div>
+                            <span className="w-full h-[40px] flex flex-row items-center justify-between bg-white rounded-b-[5px] border-t-2 border-gray-200 px-[15px] rounded-b-[5px] ">
+                            <span className="flex flex-row items-center justify-start gap-3 h-full">
+                                <p className="text-sm cursor-pointer">Prev</p>
+                                <span className="w-auto h-full flex flex-row items-center justify-start">
+                                    <p className="text-sm font-light border border-gray-400 h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">1</p>
+                                    <p className="text-sm font-light h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">2</p>
+                                    <p className="text-sm font-light h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">3</p>
+                                    <p className="text-sm font-light h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">4</p>
+
+                                </span>
+                                <p className="text-sm cursor-pointer">Next</p>
+                            </span>
+                            <span className="flex flex-row items-center justify-end gap-3 h-full">
+                                <p className="text-sm">Showing 1-5 of 60</p>
+                            </span>
+                        </span>
+                    </div>
+                    {/* sales analytics */}
+                </div>
+
+                 {/* Employee Performance */}
+                <div className="w-full h-auto flex flex-col items-start justify-start gap-[10px]">
+                    <p className="text-xl font-semibold">Employee Performance </p>
+                    <div className="w-full min-h-[150px] flex flex-col bg-white rounded-[5px] border border-blue-500 ">
+                            <span className="w-full h-[40px] flex flex-col items-start justify-start bg-white rounded-t-[5px] border-b-2 border-gray-200 ">
+                                <span className="w-full h-[40px] flex flex-row items-center justify-start bg-white rounded-t-[5px]">
+                                    <p className="text-sm font-semibold w-[20%] pr-2 pl-2 ">Employee Id</p>
+                                    <p className="text-sm font-semibold w-[20%] pr-2 pl-2 ">Employee Name</p>
+                                    <p className="text-sm font-semibold w-[20%] pr-2 pl-2 ">Role</p>
+                                    <p className="text-sm font-semibold w-[20%] pr-2 pl-2 ">Performance Score (%) </p>
+                                    <p className="text-sm font-semibold w-[20%] pr-2 pl-2 ">Last Review Date</p>
+                                </span>
+                            </span>
+                            <div className="w-full h-[200px] flex flex-col justify-start items-start">
+                                {[1,2,3,4,5].map((data, ind)=>{
+                                    return (
+                                        <span key={ind} className="recent-activity-table-list">
+                                            <p className="text-sm w-[20%] pr-2 pl-2 ">EMP001{ind}</p>
+                                            <p className="text-sm w-[20%] pr-2 pl-2 ">Iroegbu David</p>
+                                            <p className="text-sm w-[20%] pr-2 pl-2 ">Technician</p> 
+                                            <p className="text-sm w-[20%] pr-2 pl-2 text-red-600 ">75</p> 
+                                            <p className="text-sm w-[20%] pr-2 pl-2 text-lime-500 ">June 13, 2024</p> 
+                                        </span>
+                                    )
+                                })}
+                            </div>
+                            <span className="w-full h-[40px] flex flex-row items-center justify-between bg-white rounded-b-[5px] border-t-2 border-gray-200 px-[15px] rounded-b-[5px] ">
+                            <span className="flex flex-row items-center justify-start gap-3 h-full">
+                                <p className="text-sm cursor-pointer">Prev</p>
+                                <span className="w-auto h-full flex flex-row items-center justify-start">
+                                    <p className="text-sm font-light border border-gray-400 h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">1</p>
+                                    <p className="text-sm font-light h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">2</p>
+                                    <p className="text-sm font-light h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">3</p>
+                                    <p className="text-sm font-light h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">4</p>
+
+                                </span>
+                                <p className="text-sm cursor-pointer">Next</p>
+                            </span>
+                            <span className="flex flex-row items-center justify-end gap-3 h-full">
+                                <p className="text-sm">Showing 1-5 of 60</p>
+                            </span>
+                        </span>
+                    </div>
+                    {/* sales analytics */}
+                </div>
+
+                 {/* Payroll request */}
+                <div className="w-full h-auto flex flex-col items-start justify-start gap-[10px]">
+                    <p className="text-xl font-semibold">Payroll Report </p>
+                    <div className="w-full min-h-[150px] flex flex-col bg-white rounded-[5px] border border-blue-500 ">
+                            <span className="w-full h-[40px] flex flex-col items-start justify-start bg-white rounded-t-[5px] border-b-2 border-gray-200 ">
+                                <span className="w-full h-[40px] flex flex-row items-center justify-start bg-white rounded-t-[5px]">
+                                    <p className="text-sm font-semibold w-[17.5%] pr-2 pl-2 ">Employee Id</p>
+                                    <p className="text-sm font-semibold w-[17.5%] pr-2 pl-2 ">Employee Name</p>
+                                    <p className="text-sm font-semibold w-[15%] pr-2 pl-2 ">Role</p>
+                                    <p className="text-sm font-semibold w-[15%] pr-2 pl-2 ">Salary ($)</p>
+                                    <p className="text-sm font-semibold w-[20%] pr-2 pl-2 ">Payment Date</p>
+                                    <p className="text-sm font-semibold w-[15%] pr-2 pl-2 ">Status</p>
+                                </span>
+                            </span>
+                            <div className="w-full h-[200px] flex flex-col justify-start items-start">
+                                {[1,2,3,4,5].map((data, ind)=>{
+                                    return (
+                                        <span key={ind} className="recent-activity-table-list">
+                                            <p className="text-sm w-[17.5%] pr-2 pl-2 ">EMP001{ind}</p>
+                                            <p className="text-sm w-[17.5%] pr-2 pl-2 ">Iroegbu David</p>
+                                            <p className="text-sm w-[15%] pr-2 pl-2 ">Finance</p>
+                                            <p className="text-sm w-[15%] pr-2 pl-2 ">12,550</p> 
+                                            <p className="text-sm w-[20%] pr-2 pl-2 ">May 31, 2024</p> 
+                                            <p className="text-sm w-[15%] pr-2 pl-2 text-lime-500 ">Paid</p> 
+                                        </span>
+                                    )
+                                })}
+                            </div>
+                            <span className="w-full h-[40px] flex flex-row items-center justify-between bg-white rounded-b-[5px] border-t-2 border-gray-200 px-[15px] rounded-b-[5px] ">
+                            <span className="flex flex-row items-center justify-start gap-3 h-full">
+                                <p className="text-sm cursor-pointer">Prev</p>
+                                <span className="w-auto h-full flex flex-row items-center justify-start">
+                                    <p className="text-sm font-light border border-gray-400 h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">1</p>
+                                    <p className="text-sm font-light h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">2</p>
+                                    <p className="text-sm font-light h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">3</p>
+                                    <p className="text-sm font-light h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">4</p>
+
+                                </span>
+                                <p className="text-sm cursor-pointer">Next</p>
+                            </span>
+                            <span className="flex flex-row items-center justify-end gap-3 h-full">
+                                <p className="text-sm">Showing 1-5 of 60</p>
+                            </span>
+                        </span>
+                    </div>
+                    {/* sales analytics */}
+                </div>
             </div>
         </div>
     )
