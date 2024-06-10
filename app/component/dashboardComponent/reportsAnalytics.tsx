64a0d1629b42b436dd1b1fa1847505f337919db7
Iroegbu David <iroegbu.dgit@gmail.com>
2024-06-10 01:07:59 +0100

    new commit

--- app/component/dashboardComponent/reportsAnalytics.tsx
+++ app/component/dashboardComponent/reportsAnalytics.tsx
@@ -158,6 +158,52 @@ const ReportsAndAnalytics = () => {
                     {/* sales analytics */}
                 </div>
 
+                <div className="w-full h-auto flex flex-col items-start justify-start gap-[10px]">
+                    <p className="text-xl font-semibold">Installation Completion Matrics</p>
+                    <div className="w-full min-h-[150px] flex flex-col bg-white rounded-[5px] border border-blue-500 ">
+                            <span className="w-full h-[40px] flex flex-col items-start justify-start bg-white rounded-t-[5px] border-b-2 border-gray-200 ">
+                                <span className="w-full h-[40px] flex flex-row items-center justify-start bg-white rounded-t-[5px]">
+                                    <p className="text-sm font-semibold w-[20%] pr-2 pl-2 ">Installation Id</p>
+                                    <p className="text-sm font-semibold w-[15%] pr-2 pl-2 ">Project Id</p>
+                                    <p className="text-sm font-semibold w-[15%] pr-2 pl-2 ">Phase</p>
+                                    <p className="text-sm font-semibold w-[17.5%] pr-2 pl-2 ">Shart Date</p>
+                                    <p className="text-sm font-semibold w-[17.5%] pr-2 pl-2 ">End Date</p>
+                                    <p className="text-sm font-semibold w-[15%] pr-2 pl-2 ">Status</p>
+                                </span>
+                            </span>
+                            <div className="w-full h-[200px] flex flex-col justify-start items-start">
+                                {[1,2,3,4,5].map((data, ind)=>{
+                                    return (
+                                        <span key={ind} className="recent-activity-table-list">
+                                            <p className="text-sm w-[20%] pr-2 pl-2 ">INST000{ind + 1}</p>
+                                            <p className="text-sm w-[15%] pr-2 pl-2 ">PROJ000{ind + 1}</p>
+                                            <p className="text-sm w-[15%] pr-2 pl-2 ">Footing</p> {/* set post,set post, demo */}
+                                            <p className="text-sm w-[17.5%] pr-2 pl-2 ">8 hours</p>
+                                            <p className="text-sm w-[17.5%] pr-2 pl-2 ">{(ind % 2 ) === 1 ? "Created new lead" : "Updated project status"}</p>
+                                            <p className="text-sm w-[15%] pr-2 pl-2 ">Not Started</p>
+                                        </span>
+                                    )
+                                })}
+                            </div>
+                            <span className="w-full h-[40px] flex flex-row items-center justify-between bg-white rounded-b-[5px] border-t-2 border-gray-200 px-[15px] rounded-b-[5px] ">
+                            <span className="flex flex-row items-center justify-start gap-3 h-full">
+                                <p className="text-sm cursor-pointer">Prev</p>
+                                <span className="w-auto h-full flex flex-row items-center justify-start">
+                                    <p className="text-sm font-light border border-gray-400 h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">1</p>
+                                    <p className="text-sm font-light h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">2</p>
+                                    <p className="text-sm font-light h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">3</p>
+                                    <p className="text-sm font-light h-[27px] w-[30px] rounded-[3px] flex items-center justify-center cursor-pointer">4</p>
+
+                                </span>
+                                <p className="text-sm cursor-pointer">Next</p>
+                            </span>
+                            <span className="flex flex-row items-center justify-end gap-3 h-full">
+                                <p className="text-sm">Showing 1-5 of 60</p>
+                            </span>
+                        </span>
+                    </div>
+                    {/* sales analytics */}
+                </div>
             </div>
         </div>
     )
