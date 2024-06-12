f32d04d6cdb832d4b0c7d6ffcebcf3fefa8678f7
Iroegbu David <iroegbu.dgit@gmail.com>
2024-06-12 19:04:17 +0100

    new commit

--- app/component/dashboardComponent/dashboardNav.tsx
+++ app/component/dashboardComponent/dashboardNav.tsx
@@ -35,7 +35,7 @@ const DashboardNav = () => {
                     <p className='text-sm font-semibold text-white'>Iroegbu David</p>
                     <p className='text-sm text-white'>Admin</p>
                 </span>
-                <img className="inline-block h-[35px] w-[40px] rounded-[3px] ring-2 ring-white cursor-pointer"
+                <img className="inline-block h-[35px] w-[35px] rounded-full ring-2 ring-white cursor-pointer"
                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
                 </img>
                 {/* bell icon */}
