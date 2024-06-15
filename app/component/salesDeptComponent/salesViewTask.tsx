2c887d4b7fb3eed3456ba0a39574375022415e3a
Iroegbu David <iroegbu.dgit@gmail.com>
2024-06-15 08:42:04 +0100

    nwe changes

--- app/component/salesDeptComponent/salesViewTask.tsx
+++ app/component/salesDeptComponent/salesViewTask.tsx
@@ -211,7 +211,7 @@ const ViewTask = ({addTask, selectedTask, show, setShow, setSelectedTask}:SalesT
                             <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="3"></circle>
                             <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
                         </svg>
-                        ) : 'Add User'}
+                        ) : 'Add Task'}
                     </button>}
                     </span>
                 </div>
