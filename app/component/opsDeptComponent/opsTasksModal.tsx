6119709ca4ca362e11adc15f0344e0b08ff95966
unknown <iroegbu.dgit@gmail.com>
2024-08-23 07:00:58 +0100

    testing 123

--- app/component/opsDeptComponent/opsTasksModal.tsx
+++ app/component/opsDeptComponent/opsTasksModal.tsx
@@ -648,13 +648,13 @@ const Task_Management_Modal = ({ showModal, setShowModal, selectedTask, setSelec
                                                 </span>
                                             </span>
 
-                                            <button className="w-full h-[40px] text-white bg-blue-600 rounded-[5px] hover:bg-blue-700 flex items-center justify-center text-sm "  disabled={loading} onClick={update_task} >
+                                            <button className="w-full h-[40px] text-white bg-amber-700 rounded-[5px] hover:bg-amber-600 flex items-center justify-center text-sm "  disabled={loading} onClick={update_task} >
                                                 {loading ? (
                                                     <svg className="w-[25px] h-[25px] animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                                         <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="3"></circle>
                                                         <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
                                                     </svg>
-                                                ) : 'Create Task'}
+                                                ) : 'Update Task'}
 
                                             </button>
                                             
