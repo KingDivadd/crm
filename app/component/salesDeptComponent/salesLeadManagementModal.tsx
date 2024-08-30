691605e97fb696eedeb18d9ccc1c45caf6006c75
unknown <iroegbu.dgit@gmail.com>
2024-08-30 05:55:49 +0100

    new commit

--- app/component/salesDeptComponent/salesLeadManagementModal.tsx
+++ app/component/salesDeptComponent/salesLeadManagementModal.tsx
@@ -642,15 +642,15 @@ const Lead_Management_Modal = ({ showModal, setShowModal, selectedLead, setSelec
                                                 </div>
                                                 }
 
-                                                <button className=" w-full h-[40px] text-white bg-blue-600 rounded-[3px] hover:bg-blue-700 flex items-center justify-center text-sm "  disabled={loading} onClick={create_lead} >
-                                            {loading ? (
-                                                <svg className="w-[25px] h-[25px] animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="3"></circle>
-                                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
-                                                </svg>
-                                            ) : 'Create Lead'}
+                                                <button className=" w-full h-[40px] text-white bg-amber-600 rounded-[3px] hover:bg-amber-700 flex items-center justify-center text-sm "  disabled={loading} onClick={update_lead} >
+                                                    {loading ? (
+                                                        <svg className="w-[25px] h-[25px] animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="3"></circle>
+                                                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+                                                        </svg>
+                                                    ) : 'Update Lead'}
 
-                                        </button>
+                                            </button>
                                             </span>
                                         </div>
                                         
